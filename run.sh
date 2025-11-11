e7b750b63f7bad4ac0e0677baa2a21956e98415f
might <andrezito32@gmail.com>
2025-11-11 21:01:07 +0000

    Changed run.sh to run the whole pipeline

--- run.sh
+++ run.sh
@@ -8,9 +8,16 @@ echo "[2/3] Download raw collision data..."
 python ./src/import_collisions.py
 
 echo ""
-echo "[3/3] Clean and validate data..."
+echo "[3/5] Clean and validate data..."
 python ./src/clean_dataframe.py
 
+echo ""
+echo "[4/5] Running modelling scripts..."
+python ./src/modeling.py
+
+echo ""
+echo "[5/5] Launching interface..."
+python -m streamlit run ./src/heatmap.py
 echo ""
 #TODO "Add the rest of the pipeline here"
 echo "=== Pipeline Complete ==="
